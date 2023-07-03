66fb5e6dd60bf42175f41dbcc9a8504cf7bd5689
Manish Bhusal <manishbhusal6@gmail.com>
2023-07-03 12:58:09 +0545

    Added session functionality to prevent direct access to thank you page without submitting the contact form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Blog,Contact
-
 
 
 # Create your views here.
@@ -32,9 +32,22 @@ def contact(request):
         message=request.POST.get("message")
 
         Contact.objects.create(name=name,email=email,message=message)
+
+        # Set a session variable to indicate that the form has been submitted
+        request.session['submitted'] = True
+        # Redirect to the thank you page
         return redirect('thank_you')
         
     return render(request, 'blog/contact.html')
 
 def thank_you(request):
+    # Check if the session variable is set to True
+    if not request.session.get('submitted',False):
+        # If it is not, redirect to the contact page
+        messages.error(request, 'Please fill out the form first.')
+        return redirect('contact')
+    
+    # Clear the session variable
+    request.session['submitted'] = False
+
     return render(request, 'blog/thank_you.html')
